840ba127d1a9171b49e4aee99a541dd77e6f70e8
aguyonnaud <agnes.guyonnaud@insa-lyon.fr>
2025-12-18 10:12:35 +0100

    display depth

--- project/moves.pl
+++ project/moves.pl
@@ -84,7 +84,9 @@ make_move2(minimax, Player, Board, B2, Move) :-
 make_move2(minimax_scoring, Player, Board, B2, Move) :-
     nl,
     nl,
-    write('Min-max AI is thinking about next move...'),
+    maxdepth(minimax_scoring, Depth),
+    write('Min-max_scoring AI is thinking about next move... | depth : '),
+    write(Depth), nl,
     player_mark(Player, Mark),
     %utility(Board, Utility),
     minimax_scoring(0, Board,Mark,Move,Utility),
@@ -117,7 +119,9 @@ make_move2(minimax_ab, Player, Board, B2, Move) :-
 make_move2(minimax_ab_opti, Player, Board, B2, Move) :-
     nl,
     nl,
-    write('Alpha-Beta is thinking about next move...'),
+    maxdepth(minimax_ab_opti, Depth),
+    write('Alpha-Beta opti AI is thinking about next move... | depth : '),
+    write(Depth), nl,
     player_mark(Player, Mark),
     minimax_ab_opti(0, Board, Mark, Move, Utility, -10000000, 10000000),  % Alpha=-10000000, Beta=+10000000
     move(Board, Move, Mark, B2),
