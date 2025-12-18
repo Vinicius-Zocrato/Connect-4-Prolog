e3b974364517a286856f3ae09d2ba706d2c9142f
Laurent <zhu_laurent@hotmail.com>
2025-12-18 09:24:48 +0100

    deleted useless files + added vanilla minmax

---
+++ project/minimaxAI.pl
@@ -0,0 +1,145 @@
+%.......................................
+% minimax
+%.......................................
+% The minimax algorithm always assumes an optimal opponent.
+% For tic-tac-toe, optimal play will always result in a tie, so the algorithm is effectively playing not-to-lose.
+% For the opening move against an optimal player, the best minimax can ever hope for is a tie.
+% So, technically speaking, any opening move is acceptable.
+% Save the user the trouble of waiting  for the computer to search the entire minimax tree 
+% by simply selecting a random square.
+
+minimax(D, [
+        [E,E,E,E,E,E], 
+        [E,E,E,E,E,E], 
+        [E,E,E,E,E,E], 
+        [E,E,E,E,E,E], 
+        [E,E,E,E,E,E], 
+        [E,E,E,E,E,E], 
+        [E,E,E,E,E,E]
+    ],M,S,U) :-   
+    blank_mark(E),
+    U = 0,
+    S = 4,
+    !
+    .
+
+
+minimax(D, B, M, S, U) :-
+    maxdepth(MaxDepth),
+    D < MaxDepth,              %%% limit the depth of the search to avoid long computation times
+    moves(B, L),               %%% get the list of available moves
+    L \= [],                   %%% if there are available moves,
+    !,
+    D2 is D + 1,
+    best(D2, B, M, L, S, U)    %%% recursively determine the best available move
+    .
+
+% if there are no more available moves, 
+% then the minimax value is the utility of the given board position
+
+minimax(D, B, M, S, U) :-
+    utility(B, U),
+    S = 0, !
+    .
+
+
+%.......................................
+% best
+%.......................................
+% determines the best move in a given list of moves by recursively calling minimax
+%
+
+% if there is only one move left in the list...
+
+best(D,B,M,[S1],S,U) :-
+    move(B,S1,M,B2),        %%% apply that move to the board,
+    inverse_mark(M,M2), !,   
+    minimax(D, B2,M2,_S,U),  %%% then recursively search for the utility value of that move.
+    S = S1, !
+    .
+
+% if there is more than one move in the list...
+
+best(D,B,M,[S1|T],S,U) :-
+    move(B,S1,M,B2),             %%% apply the first move (in the list) to the board,
+    inverse_mark(M,M2), !,
+    minimax(D,B2,M2,_S,U1),      %%% recursively search for the utility value of that move,
+    best(D,B,M,T,S2,U2),         %%% determine the best move of the remaining moves,     
+    better(D,M,S1,U1,S2,U2,S,U), !  %%% and choose the better of the two moves (based on their respective utility values)
+    .
+
+
+%.......................................
+% better
+%.......................................
+% returns the better of two moves based on their respective utility values.
+%
+% if both moves have the same utility value, then one is chosen at random.
+
+better(D,M,S1,U1,S2,U2,     S,U) :-
+    maximizing(M),                     %%% if the player is maximizing
+    U1 > U2,                           %%% then greater is better.
+    S = S1,
+    U = U1,
+    !
+    .
+
+better(D,M,S1,U1,S2,U2,     S,U) :-
+    minimizing(M),                     %%% if the player is minimizing,
+    U1 < U2,                           %%% then lesser is better.
+    S = S1,
+    U = U1, 
+    !
+    .
+
+better(D,M,S1,U1,S2,U2,     S,U) :-
+    U1 == U2,                          %%% if moves have equal utility,
+    random_int_1n(10,R),               %%% then pick one of them at random
+    better2(D,R,M,S1,U1,S2,U2,S,U),    
+    !
+    .
+
+better(D,M,S1,U1,S2,U2,     S,U) :-        %%% otherwise, second move is better
+    S = S2,
+    U = U2,
+    !
+    .
+
+
+%.......................................
+% better2
+%.......................................
+% randomly selects two squares of the same utility value given a single probability
+%
+
+better2(D,R,M,S1,U1,S2,U2,  S,U) :-
+    R < 6,
+    S = S1,
+    U = U1, 
+    !
+    .
+
+better2(D,R,M,S1,U1,S2,U2,  S,U) :-
+    S = S2,
+    U = U2,
+    !
+    . 
+
+%.......................................
+% utility - Évalue le plateau complet
+%.......................................
+
+utility(B, U) :-
+    win(B, 'x'),
+    U = 10000, !
+    .
+
+utility(B, U) :-
+    win(B, 'o'),
+    U = (-10000), !
+    .
+
+utility(B, U) :-
+    U = 0, !
+        . 
+
